7e60c73d17627380805450fe743e9517f9d280b3
Mari <marinarosemartin@gmail.com>
2020-09-28 23:04:47 -0500

    DB Setup step

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Summer Thyme Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Jack-O-Lentil Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("As Gouda As It Gets Burger", 1);
