943cb74f0c0d938ec2a75bfa55ada190ae94728e
Mari <marinarosemartin@gmail.com>
2020-10-08 14:20:01 -0500

    changed connetion.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "",
-//     database: "burgers_db"
-// });
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
@@ -21,13 +14,7 @@ if (process.env.JAWSDB_URL) {
 }
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
