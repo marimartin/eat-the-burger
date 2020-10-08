63c9e81d656805fd19822ad4ba6df381d07cc318
Mari <marinarosemartin@gmail.com>
2020-10-07 19:30:35 -0500

    styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured) VALUES ("Summer Thyme Burger", 1);
 INSERT INTO burgers (burger_name, devoured) VALUES ("The Jack-O-Lentil Burger", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ("As sGouda As It Gets Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("As Gouda As It Gets Burger", 1);
